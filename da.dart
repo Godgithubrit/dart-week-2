79cbd6dd6be2624861cc3029b103f47b295d132c
Godgithubrit <161836869+Godgithubrit@users.noreply.github.com>
2024-03-07 16:50:48 +0300

    Add files via upload

---
+++ da.dart
@@ -0,0 +1,22 @@
+void main(){
+  //numbers which carry numerical values and can either be integer ( 10,20,1) or double(1.5, 1/3)
+  int height = 10;
+  double weight = 5.2;
+  print(height);
+  // String which store text. the text must be stored in quots '' or ""
+  String name = 'godfrey';
+  print(name);
+  // boolean which carries true or false functions
+  bool is_married = true;
+  print(is_married);
+//List which holds ordered collection of values
+  List names = ['john','ian','Dorcus','godfrey','natash'];
+  print(names);
+  // values in a list can be accesed through indexes as bellow
+  print(names[0]);
+  // Map is a collection that represent sets of value pairs.
+  Map ages = {'john':20,'alice':30,'natasha':17};
+  
+
+
+}
